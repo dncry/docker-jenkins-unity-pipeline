e4e73ef823b710c5c91842a66f31fef8048d274a
dncry <wan.shuo@qq.com>
2024-02-06 14:03:45 +0800

    init

---
+++ sh/docker_build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run \
+  -e UNITY_USERNAME \
+  -e UNITY_PASSWORD \
+  -e UNITY_SERIAL \
+  -e BUILD_VERSION \
+  -e BUILD_BUNDLECODE \
+  -e BUILD_PATH \
+  -e BUILD_SEPARATEASSET \
+  -e BUILD_BUILDAAB \
+  -e BUILD_TARGET \
+  -e BUILD_METHOD \
+  -w /project/ \
+  -v $WORKSPACE_OS:/project:cached \
+  -v $GRADLECACHE_OS:/root/.gradle \
+  $IMAGE \
+  /bin/bash -c "${CI_DIR_UNITY}/before_script.sh && ${CI_DIR_UNITY}/build.sh"

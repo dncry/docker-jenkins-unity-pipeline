7cb5a1f218bf5d7d13f9d5ea638a596b5feaf098
dncry <wan.shuo@qq.com>
2024-07-17 10:53:43 +0800

    Revert "Update build.sh"
    This reverts commit a73498ae196c6525565e3d1467b4474fa54f3726.

--- sh/build.sh
+++ sh/build.sh
@@ -19,7 +19,7 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -buildBundleCode $BUILD_BUNDLECODE \
   -buildPath $BUILD_PATH \
   -buildSeparateAsset $BUILD_SEPARATEASSET \
-  -buildAndroidType $BUILD_BuildAndroidType \
+  -buildAAB $BUILD_BUILDAAB \
   -buildTarget $BUILD_TARGET \
   -sdkPath $Sdk_Path_Env \
   -ndkPath $Ndk_Path_Env \

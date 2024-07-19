c871d4db81e8cde8ea94426814f8774ade11cd70
dncry <wan.shuo@qq.com>
2024-07-19 09:57:29 +0800

    2

--- sh/docker_android_build.sh
+++ sh/docker_android_build.sh
@@ -76,7 +76,7 @@ find "$BUILD_PATH_Jenkins" -type f \( -name "*.apk" -o -name "*.aab" \) -exec ba
 
         build_version= $BUILD_VERSION
 
-        new_filename="${file_name_without_extension}_${BUILD_VERSION}_${timestamp}.${extension}"
+        new_filename="${file_name_without_extension}_${FILE_NAME}_${BUILD_VERSION}_${timestamp}.${extension}"
 
         # 构建新的文件路径（保持原目录结构不变）
 

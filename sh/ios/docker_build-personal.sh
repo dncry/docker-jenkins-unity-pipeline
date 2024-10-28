bda5b68bab9e447f1c2d52de52fef8a0118cd142
dncry <wan.shuo@qq.com>
2024-10-28 13:57:10 +0800

    2

--- sh/ios/docker_build-personal.sh
+++ sh/ios/docker_build-personal.sh
@@ -9,22 +9,10 @@ docker run \
   -e BUILD_VERSION \
   -e BUILD_BUNDLECODE \
   -e BUILD_PATH \
-  -e BUILD_SEPARATEASSET \
-  -e BUILD_BUILDAAB \
-  -e BUILD_BUILDEXPORTPROJECT \
   -e BUILD_TARGET \
   -e BUILD_METHOD \
-  -e Sdk_Path_Env \
-  -e Ndk_Path_Env \
-  -e Jdk_Path_Env \
-  -e Gradle_Path_Env \
   -w /project/ \
   -v $WORKSPACE_OS:/project:cached \
-  -v $GRADLECACHE_OS:/root/.gradle \
-  -v $Sdk_Path_OS:$Sdk_Path_UNITY \
-  -v $Ndk_Path_OS:$Ndk_Path_UNITY \
-  -v $Jdk_Path_OS:$Jdk_Path_UNITY \
-  -v $Gradle_Path_OS:$Gradle_Path_UNITY \
   --rm \
   $IMAGE \
   /bin/bash -c "${CI_DIR_UNITY_ShGit}/build-personal.sh"
